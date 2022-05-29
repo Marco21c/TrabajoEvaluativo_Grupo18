876b2edf8349226f56d27d803a0a3e04971f5911
ariel <ariel@DESKTOP-MNIDRE2>
2022-05-29 20:54:18 -0300

    se agrega paquete service, service imp, y sus metodos correspondiente

---
+++ TrabajoEvaluativo_Grupo18/src/main/java/ar/edu/unju/fi/html/service/IUsuarioService.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.html.service;
+
+import ar.edu.unju.fi.html.model.Usuario;
+import ar.edu.unju.fi.html.util.ListaUsuarios;
+
+public interface IUsuarioService {
+	public Usuario getUsuario();
+	public boolean guardarUsuario(Usuario usuario);
+	public ListaUsuarios getListaUsuarios();
+}
--- TrabajoEvaluativo_Grupo18/src/main/java/ar/edu/unju/fi/html/util/ListaUsuarios.java
+++ TrabajoEvaluativo_Grupo18/src/main/java/ar/edu/unju/fi/html/util/ListaUsuarios.java
@@ -2,14 +2,26 @@ package ar.edu.unju.fi.html.util;
 
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Component;
+
 import ar.edu.unju.fi.html.model.Usuario;
 import java.time.LocalDate;
-
+@Component
 public class ListaUsuarios {
 	ArrayList<Usuario> usuarios;
 	
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
 	public ListaUsuarios() {
 		usuarios = new ArrayList<Usuario>();
 		usuarios.add(new Usuario("pepe", "arg@mail", LocalDate.of(24, 03, 1999)));
 	}
+	
+	
 }
