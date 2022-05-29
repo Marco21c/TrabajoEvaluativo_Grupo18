33f3421ef43ffc4579a2a242e58b0e0671dac6e7
ariel <ariel@DESKTOP-MNIDRE2>
2022-05-29 19:46:35 -0300

    se crea lista de candidatos

--- TrabajoEvaluativo_Grupo18/src/main/java/ar/edu/unju/fi/html/util/ListaCandidatos.java
+++ TrabajoEvaluativo_Grupo18/src/main/java/ar/edu/unju/fi/html/util/ListaCandidatos.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.fi.html.util;
 
-public class ListaCandidatos {
+import java.util.ArrayList;
 
+import ar.edu.unju.fi.html.model.Candidato;
+
+public class ListaCandidatos {
+	ArrayList<Candidato> candidatos;
+	
+	public ListaCandidatos() {
+		candidatos = new ArrayList<Candidato>();
+		candidatos.add(new Candidato(1, "ninguno", "-", "-", 0));
+		candidatos.add(new Candidato(1, "metallica", "thrash metal", "banda de rock metal", 0));
+		
+	}
 }
