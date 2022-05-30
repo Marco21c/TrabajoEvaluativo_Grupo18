e935794070a9cf596a35d161eb1c51917d5d545b
ariel <ariel@DESKTOP-MNIDRE2>
2022-05-29 23:26:39 -0300

    se agregaron los metodos para candidato

--- TrabajoEvaluativo_Grupo18/src/main/java/ar/edu/unju/fi/html/service/imp/CandidatoServicelmp.java
+++ TrabajoEvaluativo_Grupo18/src/main/java/ar/edu/unju/fi/html/service/imp/CandidatoServicelmp.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi.html.service.imp;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ar.edu.unju.fi.html.model.Candidato;
@@ -9,7 +11,7 @@ import ar.edu.unju.fi.html.util.ListaCandidato;
 public class CandidatoServicelmp implements ICandidatoService {
 	
 	@Autowired
-	private ListaCandidato listacandidatos;
+	private ListaCandidato listaCandidato;
 
 	@Override
 	public Candidato getCandidado() {
@@ -20,31 +22,42 @@ public class CandidatoServicelmp implements ICandidatoService {
 	@Override
 	public Boolean guardarCandidato(Candidato candidato) {
 		// TODO Auto-generated method stub
-		return listacandidatos.getCandidatos().add(candidato);
+		return listaCandidato.getCandidatos().add(candidato);
 	}
 
 	@Override
 	public void modificarCandidato(Candidato candidato) {
 		// TODO Auto-generated method stub
+		for(Candidato can: listaCandidato.getCandidatos()) {
+			if(can.getCodigo() == candidato.getCodigo()) {
+				can.setNombre(candidato.getNombre());
+				can.setGenero_music(candidato.getGenero_music());
+				can.setDescripcion(candidato.getDescripcion());
+				can.setCant_votos(candidato.getCant_votos());
+			}
+		}
 
 	}
 
 	@Override
 	public void eliminarCandidato(int codigo) {
 		// TODO Auto-generated method stub
+		Optional<Candidato> candidato = listaCandidato.getCandidatos().stream().filter(c -> c.getCodigo() == codigo).findFirst();
+		listaCandidato.getCandidatos().remove(candidato.get());
 
 	}
 
 	@Override
 	public ListaCandidato getListaCandidatos() {
 		// TODO Auto-generated method stub
-		return null;
+		return listaCandidato;
 	}
 
 	@Override
 	public Candidato buscarCandidato(int codigo) {
 		// TODO Auto-generated method stub
-		return null;
+		Optional<Candidato> candidato = listaCandidato.getCandidatos().stream().filter(c -> c.getCodigo() == codigo).findFirst();
+		return candidato.get();
 	}
 
 }
